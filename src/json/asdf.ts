5e9c16b4f6423e21d3677a73b4c2c834cf6a595f
Fajar Maulana <fajarma21@gmail.com>
2025-05-19 15:50:38 +0700

    fix(types): add d.ts file

--- src/json/asdf.ts
+++ src/json/asdf.ts
@@ -1,7 +1,7 @@
-function JsonParse<T>(value: string): T {
+function jsonParse<T>(value: string): T {
   let finalValue = value || '{}';
   if (typeof finalValue !== 'string') finalValue = '{}';
   return JSON.parse(finalValue);
 }
 
-export default JsonParse;
+export default jsonParse;
