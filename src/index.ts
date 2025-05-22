51147748032e8114252cd05e98e420cecc009468
Fajar Maulana <fajarma21@gmail.com>
2025-05-22 16:51:52 +0700

    feat(index): put all utils in index

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// DOM
+import canUseDOM from './dom/canUseDOM';
+
+// JSON
+import jsonParse from './json/jsonParse';
+
+// number
+import roundNumber from './number/roundNumber';
+
+// storage
+import getLS from './storage/getLS';
+import setLS from './storage/setLS';
+
+export { canUseDOM, jsonParse, roundNumber, getLS, setLS };
--- src/number/roundNumber.ts
+++ src/number/roundNumber.ts
@@ -1,6 +1,6 @@
-const getRoundNumber = (value: number, decimalCount = 0) => {
+const roundNumber = (value: number, decimalCount = 0) => {
   const decimalValue = Math.pow(10, decimalCount);
   return Math.round(value * decimalValue) / decimalValue;
 };
 
-export default getRoundNumber;
+export default roundNumber;
